2c0c29b8d493d044cea1bd5bf55dc9ef973b3259
Kelli Watkins <watkinskellil@gmail.com>
2022-10-26 20:12:11 -0600

    Add work

--- arrays.js
+++ arrays.js
@@ -1,3 +1,10 @@
 var names = ["jess", "james", "joe", "jimmy"];
 var areaCodes = [405, 580, 719, 220];
-var booleans = [true, false, true, false]
+var booleans = [true, false, true, false];
+
+names.pop();
+console.log(names);
+areaCodes.shift();
+console.log(areaCodes);
+booleans.unshift(true)
+console.log(booleans)
